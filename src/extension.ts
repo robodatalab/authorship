7d05e0fac880b1c57b3d6ee1ef1d07647bd3705d
Piotr Trochim <ptrochim@gmail.com>
2026-07-18 18:48:18 +0200

    starting the extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,34 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// This method is called when your extension is activated, which happens the
+// first time the Authorship view becomes visible.
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "authorship" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('authorship.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Authorship!');
-	});
+	// The view container and view are declared in package.json under
+	// contributes.viewsContainers / contributes.views. Registering the provider
+	// here is what makes the view render; VS Code activates the extension
+	// automatically the first time the view becomes visible.
+	context.subscriptions.push(
+		vscode.window.registerTreeDataProvider(
+			'authorship.manuscript',
+			new ManuscriptProvider()
+		)
+	);
+}
 
-	context.subscriptions.push(disposable);
+class ManuscriptProvider implements vscode.TreeDataProvider<string> {
+	getTreeItem(element: string): vscode.TreeItem {
+		return new vscode.TreeItem(element, vscode.TreeItemCollapsibleState.None);
+	}
+
+	getChildren(element?: string): string[] {
+		return element ? [] : ['Chapter One', 'Chapter Two'];
+	}
 }
 
 // This method is called when your extension is deactivated
