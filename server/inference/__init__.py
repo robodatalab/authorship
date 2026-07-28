a8719d016a6d544042778c6a2b7d23a77b448b99
Piotr Trochim <ptrochim@gmail.com>
2026-07-28 17:57:15 +0200

    encoder model

--- server/inference/__init__.py
+++ server/inference/__init__.py
@@ -3,5 +3,6 @@ from server.inference.resource_manager import (
     ModelNotAvailable,
 )
 from server.inference.causal import CausalModel
+from server.inference.encoder import EncoderModel
 from server.inference.seq2seq import Seq2SeqModel
 from server.inference.utils import coedit_prompt, machine_memory, qwen_chat_prompt
