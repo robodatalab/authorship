3170f620009c9d196dc39d4e8bed24b423638ff0
Piotr Trochim <ptrochim@gmail.com>
2026-09-08 19:28:57 +0100

    image cell renderer update

---
+++ extension/webview/cell_types/ImageCell.tsx
@@ -0,0 +1,73 @@
+import {
+    AuthorFileEditorCell,
+    AuthorFileEditorCellHeader,
+    AuthorFileEditorCellBody,
+    AuthorFileEditorCellFooter,
+    AuthorFileEditorCellCard,
+} from "../author_editor/AuthorFileEditorCell";
+import { AuthorFileEditorCellFields } from "../author_editor/AuthorFileEditorCellFields";
+import type { AuthorFileEditorCellField } from "../author_editor/AuthorFileEditorCellFields";
+import { registerAuthorDocumentCellType } from "../../vscode_runtime/commands/author_document_cell_types";
+import {
+    invokeAuthorDocumentCommand,
+    type SendMessagesToVscode,
+    type WebviewCell,
+} from "../author_editor/AuthorFileEditorCanvas";
+import { IMAGE } from "../../vscode_runtime/storydoc/model";
+
+const FIELDS: AuthorFileEditorCellField[] = [
+    { attributeName: "src", label: "Image", placeholder: "cover.jpg" },
+    {
+        attributeName: "full-page",
+        label: "On a page of its own",
+        isCheckbox: true,
+    },
+];
+
+interface ImageCellProps {
+    cell: WebviewCell;
+    cellId: string;
+    sendMessagesToVscode: SendMessagesToVscode;
+}
+
+export function ImageCell({
+    cell,
+    cellId,
+    sendMessagesToVscode,
+}: ImageCellProps) {
+    return (
+        <AuthorFileEditorCell>
+            <AuthorFileEditorCellHeader>Image</AuthorFileEditorCellHeader>
+            <AuthorFileEditorCellBody>
+                <AuthorFileEditorCellCard>
+                    <AuthorFileEditorCellFields
+                        fields={FIELDS}
+                        cellAttributes={cell.attrs}
+                        onAttributeChanged={(attributeName, attributeValue) =>
+                            invokeAuthorDocumentCommand(
+                                sendMessagesToVscode,
+                                "replaceAttribute",
+                                { cellId, attributeName, attributeValue },
+                            )
+                        }
+                    />
+                </AuthorFileEditorCellCard>
+            </AuthorFileEditorCellBody>
+            <AuthorFileEditorCellFooter></AuthorFileEditorCellFooter>
+        </AuthorFileEditorCell>
+    );
+}
+
+registerAuthorDocumentCellType({
+    cellKind: IMAGE,
+    menuLabel: "Image",
+    insertMenuGroup: "secondary",
+    render: (cell, cellId, sendMessagesToVscode) => (
+        <ImageCell
+            cell={cell}
+            cellId={cellId}
+            sendMessagesToVscode={sendMessagesToVscode}
+        />
+    ),
+    newCell: () => ({ kind: IMAGE, source: "", attrs: { src: "" } }),
+});
